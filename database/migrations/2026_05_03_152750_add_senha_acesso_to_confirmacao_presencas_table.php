6c436c9c7efa2339bb0ccf0db6496e9025d0c1ae
Matheus Vinicius <matheus2vandrade@gmail.com>
2026-05-03 16:12:47 -0300

    Updated 2026-05-03 16:12:47

--- database/migrations/2026_05_03_152750_add_senha_acesso_to_confirmacao_presencas_table.php
+++ database/migrations/2026_05_03_152750_add_senha_acesso_to_confirmacao_presencas_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('confirmacao_presencas', function (Blueprint $table) {
-            //
+            $table->string('senha_acesso', 10)->nullable()->after('status');
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('confirmacao_presencas', function (Blueprint $table) {
-            //
+            $table->dropColumn('senha_acesso');
         });
     }
 };
